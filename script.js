6a706e85e209f968e7065490b5cc28dad35809ef
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2022-08-22 18:18:29 -0300

    function troca

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let btn_sim = document.getElementById("sim");
 let naos_list = [];
-for (let i = 0; i < 10; i++) {
+
+for (let i = 0; i < 30; i++) {
     naos_list[i] = document.getElementById("nao"+i);
   } 
 
-let btn_nao = naos_list[Math.floor(Math.random() * 10)];
+let btn_nao = naos_list[Math.floor(Math.random() * 30)];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 30; i++) {
     if (naos_list[i] != btn_nao){
         naos_list[i].style.cssText =
         'color: red;' +
@@ -20,6 +21,7 @@ btn_sim.onclick = function() {yesFunction()};
 function yesFunction() {
     document.getElementById("container").innerHTML = `<h1> Você escolheu a melhor opção!</h1>
     <span style='font-size:100px;'>&#128152;</span>`;
+
 console.log("click")
 }
 
@@ -29,17 +31,23 @@ function troca(){
     'color: black;' +
     'background-color: white;';
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 30; i++) {
         if (naos_list[i] != btn_nao){
             naos_list[i].style.cssText =
             'color: red;' +
             'background-color: red;';
         }    
     }
-    location.reload()
+    let anterior = btn_nao;
+    btn_nao = naos_list[Math.floor(Math.random() * 30)];
+   
+    if (anterior = btn_nao){
+        btn_nao = naos_list[Math.floor(Math.random() * 30)];
+    }
+    btn_nao.onclick = function() {troca()};
+    console.log(btn_nao);
 }
 
 
 console.log(naos_list);
-
 console.log(btn_nao);
